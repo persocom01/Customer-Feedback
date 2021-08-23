c8ea3d8dc04500bf79acf1694fc713fb6fd7d5c7
persocom01 <persocom02@gmail.com>
2021-08-23 11:07:39 +0800

    0.02

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-df = pd.read_csv('Reviews.csv')
+df = pd.read_csv('./sentiment_analysis/data/Reviews.csv')
 df.head()
